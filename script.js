5ebacb3c6fde1bd082fe636d34099ed3997a14e9
Anurag <iamanuragparashar@gmail.com>
2024-05-18 23:14:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+
+
+var car = gsap.to(".car_own_cont", {
+    x: "-99.5vw",
+    duration: "12",
+    repeat: -1,
+    ease: "linear"
+
+})
+
+var carown = document.querySelector(".car_own_cont");
+carown.addEventListener("mouseenter", () => {
+    car.pause();
+})
+carown.addEventListener("mouseleave", () => { 
+    car.play();
+})
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
+
